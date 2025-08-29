ee9e43154f8825581323998c94a5dd797969456d
reyhane <bastarz558@gmail.com>
2025-08-29 12:38:06 +0430

    Hello-world

---
+++ app/layout.tsx
@@ -0,0 +1,15 @@
+import './style.css'
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className="container mx-auto py-10" >
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function HomePage() {
+  return <div className="text-red-500">Hello World !</div>;
+}
+
+export default HomePage;
